2f84e69b9e6e9f77679bcb2d72eece269b553979
Lunac <msten75@gmail.com>
2020-12-22 15:41:45 +0100

    Auth "fixed"

--- online-doctor-appointment-system/src/main/java/com/example/demo/controller/PatientsController.java
+++ online-doctor-appointment-system/src/main/java/com/example/demo/controller/PatientsController.java
@@ -1,55 +1,34 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Patient;
 import com.example.demo.service.PatientService;
 
-@RequestMapping(value = "/api/v1/patient")
+@RequestMapping(value = "/patient")
 @RestController
-@Validated
+
+//@Validated
 public class PatientsController {
 
 	@Autowired
 	private PatientService patientService;
 
 	
-	@PreAuthorize("hasAuthority('USER') OR hasAuthority('ADMIN')")
+//	@PreAuthorize("hasAuthority('USER') OR hasAuthority('ADMIN')")
+	@CrossOrigin(origins = { "http://localhost:3000"})
 	@GetMapping(value = "/")
 	public List<Patient> getAllPatients() {
 		return patientService.getAllPatients();
 	}
 	
-	@PreAuthorize("hasAuthority('USER') OR hasAuthority('ADMIN')")
+	//@PreAuthorize("hasAuthority('USER') OR hasAuthority('ADMIN')")
 	@GetMapping(value = "/test")
 	public String test() {
 		return "test";
