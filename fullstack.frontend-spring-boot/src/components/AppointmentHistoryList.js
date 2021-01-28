4fccd78f43785cb668087fd3d24f541e2fbf23f6
Lunac <msten75@gmail.com>
2021-01-28 15:09:53 +0100

    Export csv function added

--- fullstack.frontend-spring-boot/src/components/AppointmentHistoryList.js
+++ fullstack.frontend-spring-boot/src/components/AppointmentHistoryList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import { CSVLink, CSVDownload } from "react-csv";
 
 class AppointmentHistoryList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      tempArray: []
+    };
   }
 
   appointmentHistoryList = () => {
@@ -30,7 +35,51 @@ class AppointmentHistoryList extends Component {
     });
   };
   render() {
-    return <div>{this.appointmentHistoryList()}</div>;
+    let { data, tempArray } = this.state;
+    tempArray = [
+      [
+        "Patient firstName",
+        "Patient lastName",
+        "Date",
+        "Time",
+        "Treated ailment",
+        "Phone",
+        "Email",
+        "Doctor journal",
+        "Patient feedback"
+      ]
+    ];
+    this.props.appointmentHistory.map((array, i) => {
+      tempArray.push([
+        array.patientInformation.patientFirstName,
+        array.patientInformation.patientLastName,
+        array.bookingDate,
+        array.bookingStartTime,
+        array.treatedAilment,
+        array.patientInformation.patientPhone,
+        array.patientInformation.patientEmail,
+        array.doctorFeedback,
+        array.patientFeedback
+      ]);
+    });
+
+    // const headers = [
+    //   { label: "Patient firstName", key: "patientFirstName" },
+    //   { label: "Patient lastName", key: "patientLastName" },
+    //   { label: "Date", key: "bookingDate" },
+    //   { label: "Time", key: "bookingStartTime" },
+    //   { label: "Treated ailment", key: "treatedAilment" },
+    //   { label: "Phone", key: "patientPhone" },
+    //   { label: "Email", key: "patientEmail" },
+    //   { label: "Doctor journal", key: "doctorFeedback" },
+    //   { label: "Patient feedback", key: "patientFeedback" }
+    // ];
+    return (
+      <>
+        <div>{this.appointmentHistoryList()}</div>;
+        <CSVLink data={tempArray}>Download Summary</CSVLink>;
+      </>
+    );
   }
 }
 
